c25c4b2970a14c465dd9848037217bfb9f5a3442
Fox <msalva@intricom.es>
2022-06-17 09:10:06 +0200

    Revert "multi user tests for linux"
    This reverts commit d6ff813c086b57344c45d04762a4d9d70fbb8786.

--- examples/multi-user/index.js
+++
@@ -1,9 +0,0 @@
-const os = require("os");
-
-const username =
-  process.platform === "win32"
-    ? `${process.env.USERDOMAIN}_${process.env.USERNAME}`
-    : os.userInfo().username;
-
-// Tests will check that username its the correct user
-console.log(username);
